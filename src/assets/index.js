32a2ee17115dc36330568850561bd572c52e5a27
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-03-25 22:11:44 +0300

    Remove extra files

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import "../src/styles/main.css";
-import renderLayout from "./ui/layout";
-
-renderLayout();
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,5 +5,6 @@ module.exports = merge(common, {
   mode: "production",
   output: {
     publicPath: "/phoenix-travails/",
+    clean: true,
   },
 });
